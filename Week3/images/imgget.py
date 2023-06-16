bee22e1215429252bb7e7a7feb92474b3e6c91a5
IoI_xD <alphaproject217@gmail.com>
2023-06-15 19:45:31 -0700

    midterm draft 1

--- Week3/images/imgget.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-from bing_image_downloader import downloader
-
-while True:
-    query_string = input("Enter a term: ")
-    downloader.download(query_string, limit=1, output_dir='.', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
