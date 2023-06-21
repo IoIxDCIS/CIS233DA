8e75f6a8fd25b32264c2771ac71e4091f2128451
IoI_xD <alphaproject217@gmail.com>
2023-06-20 21:46:33 -0700

    yeah

--- Week4/launchControl/assets/scripts/launch.js
+++ Week4/launchControl/assets/scripts/launch.js
@@ -2,5 +2,27 @@ let launchStatus = false;
 let errorMessage = "";
 
 function tryLaunch() {
-
+    if (brakeLevel == 0 && tracLevel == 0 && gasLevel == 1 && battLevel == 1 && gearshiftStatus == 3) {
+        document.getElementById("launch").src = "./assets/images/lca1.png";
+        errorMessage = "";
+        let audio = new Audio("./assets/media/launch.mp3");
+        audio.play();
+    } else {
+        launchStatus = false;
+        if(brakeLevel == 1) {
+            errorMessage = "Parking brake must be disengaged."
+        } else if (tracLevel == 1) {
+            errorMessage = "Tracition control must be off."
+        } else if (gasLevel == 0) {
+            errorMessage = "You have no gas."
+        } else if (battLevel == 0) {
+            errorMessage = "Battery is discharged."
+        } else if (gearshiftStatus != 3) {
+            errorMessage = "Car must be in SPORT mode."
+        }
+        document.getElementById("launch").src = "./assets/images/lca0.png";
+        let audio = new Audio("./assets/media/sputter.mp3");
+        audio.play();
+    }
+    document.getElementById("msg").innerText = errorMessage;
 }
