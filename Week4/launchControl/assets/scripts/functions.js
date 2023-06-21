8589818ec62ee256eaae8ad9fdb4a8a65da74c92
IoI_xD <alphaproject217@gmail.com>
2023-06-20 20:58:24 -0700

    Practice4a and Coding4a first

--- Week4/launchControl/assets/scripts/functions.js
+++ Week4/launchControl/assets/scripts/functions.js
@@ -13,6 +13,46 @@ function getGas() {
     }
 }
 
+let brakeLevel = 0;
+function getBrake() {
+    if(brakeLevel == 0) {
+        brakeLevel = 1;
+        document.getElementById("brake").src = "./assets/images/p0.png";
+        document.getElementById("bttn_park").src = "./assets/images/bttn_park_0.png";
+    } else if (brakeLevel == 1) {
+        brakeLevel = 0;
+        document.getElementById("brake").src = "./assets/images/p1.png";
+        document.getElementById("bttn_park").src = "./assets/images/bttn_park_1.png";
+    }
+}
+
+let tracLevel = 0;
+function getTrac() {
+    if(tracLevel == 0) {
+        tracLevel = 1;
+        document.getElementById("trac").src = "./assets/images/trac1.png";
+        document.getElementById("bttn_trac").src = "./assets/images/bttn_trac_0.png";
+    } else if (tracLevel == 1) {
+        tracLevel = 0;
+        document.getElementById("trac").src = "./assets/images/trac0.png";
+        document.getElementById("bttn_trac").src = "./assets/images/bttn_trac_1.png";
+    }
+}
+
+let battLevel = 0;
+function getBatt() {
+    if(battLevel == 0) {
+        battLevel = 1;
+        document.getElementById("batt").src = "./assets/images/batt1.png";
+        document.getElementById("bttn_batt").src = "./assets/images/bttn_batt_1.png";
+    } else {
+        battLevel = 0;
+        document.getElementById("batt").src = "./assets/images/batt0.png";
+        document.getElementById("bttn_batt").src = "./assets/images/bttn_batt_0.png";
+    }
+}
+
+
 const levels = new Map();
 
 function cycleImage(id) {
