08ce31ab4601bf34e439da7bed53722da08cb18c
IoI_xD <alphaproject217@gmail.com>
2023-06-26 18:48:46 -0700

    javascript instead of css

---
+++ Week5/scripts/weather.js
@@ -0,0 +1,22 @@
+function getCity(cityName) {
+    switch(cityName) {
+        case "arizona": 
+            document.body.id = "arizona";
+            break;
+        case "phoenix": 
+            document.body.id = "phoenix";
+            break;
+        case "mesa": 
+            document.body.id = "mesa";
+            break;
+        case "yuma": 
+            document.body.id = "yuma";
+            break;
+        case "bisbee": 
+            document.body.id = "bisbee";
+            break;
+        case "tuscon":
+            document.body.id = "tuscon";
+            break;
+    }
+}
