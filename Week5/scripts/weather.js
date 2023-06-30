6e601d5ad46185b43e26aafbccdb8e3a0db276c5
IoI_xD <alphaproject217@gmail.com>
2023-06-29 21:05:52 -0700

    final

--- Week5/scripts/weather.js
+++ Week5/scripts/weather.js
@@ -4,6 +4,14 @@ function getWeather(cityName) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey+"&units=imperial").then(response => response.json()).then(json => {
         document.querySelector("#weather_num_f").innerHTML = parseInt(json.main.temp);
         document.querySelector("#weather_num_c").innerHTML = parseInt((+(json.main.temp) - 32) / 1.8);
+
+        document.querySelector("#feels_like").innerHTML = "Feels like: "+parseInt(json.main.feels_like)+"°F" + "/" + parseInt((+(json.main.feels_like) - 32) / 1.8)+"°C";
+        document.querySelector("#temp_min").innerHTML = "Min. temp: "+parseInt(json.main.temp_min)+"°F" + "/" + parseInt((+(json.main.temp_min) - 32) / 1.8)+"°C";
+        document.querySelector("#temp_max").innerHTML = "Max temp: "+parseInt(json.main.temp_max)+"°F" + "/" + parseInt((+(json.main.temp_max) - 32) / 1.8)+"°C";
+        document.querySelector("#grnd_level").innerHTML = "Ground level: "+parseInt(json.main.grnd_level)+"ft" + "/" + parseInt(json.main.grnd_level / 3.281)+"m";
+        document.querySelector("#humidity").innerHTML = "Humidity: "+parseInt(json.main.humidity)+"%";
+        document.querySelector("#pressure").innerHTML = "Pressure: "+json.main.pressure;
+
     })
 }
 
